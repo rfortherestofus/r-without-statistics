c59930ce849f61017184ea4638f2c1c06813a860
David Keyes <david@rfortherestofus.com>
2022-08-11 14:34:12 -0700

    All code working to generate No Starch figures

--- _common.R
+++ _common.R
@@ -8,34 +8,43 @@ knitr::opts_chunk$set(
   warning = FALSE,
   message = FALSE,
   echo = FALSE,
-  dpi = 300,
-  fig.width = 4.675,
+  dpi = 150,
+  out.width = "100%",
   dev = "ragg_png"
 )
 
 
 # Functions ---------------------------------------------------------------
 
-save_figure_for_nostarch <- function(figure_height = 4) {
+save_figure_for_nostarch <- function(figure_height = 4, chap_number = chapter_number) {
   
+  chapter_number_two_digits <- stringr::str_pad(chap_number, 2, "left", "0")
   figure_number_three_digits <- stringr::str_pad(i, 3, "left", "0")
   
+  file_name <- str_glue("nostarch/figures/F{chapter_number_two_digits}{ figure_number_three_digits }.pdf")
+  
   ggsave(plot = last_plot(),
-         filename = str_glue("nostarch/figures/fCHAPTERNUMBER{ figure_number_three_digits }.pdf"),
+         filename = file_name,
          device = cairo_pdf,
          width = 4.675,
          height = figure_height)
   
   i <<- i + 1
   
+  # file_name_without_path <- str_remove(file_name, "nostarch/figures/")
+  # file_name_without_path <- str_glue("[{file_name_without_path}]")
+  # 
+  # cat(file_name_without_path)
+  
 }
 
-save_image_for_nostarch <- function(image_file) {
+save_image_for_nostarch <- function(image_file, chap_number = chapter_number) {
   
+  chapter_number_two_digits <- stringr::str_pad(chap_number, 2, "left", "0")
   figure_number_three_digits <- stringr::str_pad(i, 3, "left", "0")
   
   fs::file_copy(path = image_file,
-                new_path = str_glue("nostarch/figures/fCHAPTERNUMBER{ figure_number_three_digits }.png"),
+                new_path = str_glue("nostarch/figures/F{chapter_number_two_digits}{ figure_number_three_digits }.png"),
                 overwrite = TRUE)
   
   knitr::include_graphics(image_file)
