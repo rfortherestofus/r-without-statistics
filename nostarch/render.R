069c2af249f858d4296a54e2532185dec1e79f09
David Keyes <david@rfortherestofus.com>
2022-08-11 14:52:45 -0700

    Try GH action again

--- nostarch/render.R
+++ nostarch/render.R
@@ -1,1 +1,8 @@
+# Everything
+rmarkdown::render_site()
+
+# Website only
+rmarkdown::render_site(output_format = 'bookdown::bs4_book', encoding = 'UTF-8')
+
+# Just Word
 rmarkdown::render_site(output_format = 'bookdown::word_document2', encoding = 'UTF-8')
