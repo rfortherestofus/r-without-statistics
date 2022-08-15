d03e412a6a164e088d184e3ff92db0be4f038cfd
David Keyes <david@rfortherestofus.com>
2022-08-15 11:19:34 -0700

    Separate out functions to create and print no starch file names

--- _common.R
+++ _common.R
@@ -16,13 +16,36 @@ knitr::opts_chunk$set(
 
 # Functions ---------------------------------------------------------------
 
-save_figure_for_nostarch <- function(figure_height = 4, chap_number = chapter_number, print_figure_number = FALSE) {
+i <- 1
+
+create_nostarch_file_name <- function(file_type = "pdf", chap_number = chapter_number, print_figure_number = FALSE) {
   
   chapter_number_two_digits <- stringr::str_pad(chap_number, 2, "left", "0")
   figure_number_three_digits <- stringr::str_pad(i, 3, "left", "0")
   
-  file_name <- str_glue("F{chapter_number_two_digits}{ figure_number_three_digits }.pdf")
+  file_name <- stringr::str_glue("F{chapter_number_two_digits}{ figure_number_three_digits }.{file_type}")
+  
+  i <<- i + 1
+  
+  file_name
+  
+}
+
+print_nostarch_file_name <- function(file_type_to_print = "pdf") {
+  
+  file_name_with_brackets <- stringr::str_glue("[{create_nostarch_file_name(file_type = file_type_to_print)}]")
+  
+  cat(file_name_with_brackets)
+  
+}
+
+
+create_nostarch_file_name(chap_number = 2)
+
+save_figure_for_nostarch <- function(figure_height = 4) {
+  
   save_directory <- here::here("nostarch/figures/")
+  file_name <- create_nostarch_file_name()
   
   ggsave(plot = last_plot(),
          path = save_directory,
@@ -30,39 +53,20 @@ save_figure_for_nostarch <- function(figure_height = 4, chap_number = chapter_nu
          device = cairo_pdf,
          width = 4.675,
          height = figure_height)
-  
-  i <<- i + 1
-  
-  if (print_figure_number == TRUE) {
-    
-    file_name_with_brackets <- str_glue("[{file_name}]")
-    
-    cat(file_name_with_brackets, sep = "\n")
-    
-  }
+
   
 }
 
-save_image_for_nostarch <- function(image_file, chap_number = chapter_number, print_figure_number = FALSE) {
+save_image_for_nostarch <- function(image_file, chap_number = chapter_number) {
   
   chapter_number_two_digits <- stringr::str_pad(chap_number, 2, "left", "0")
   figure_number_three_digits <- stringr::str_pad(i, 3, "left", "0")
   
-  file_name <- str_glue("F{chapter_number_two_digits}{ figure_number_three_digits }.png")
+  file_name <- create_nostarch_file_name(file_type = "png")
   save_directory <- here::here("nostarch/figures/")
   
   fs::file_copy(path = image_file,
                 new_path = str_glue("{save_directory}/{file_name}"),
                 overwrite = TRUE)
-  
-  i <<- i + 1
-  
-  if (print_figure_number == TRUE) {
-    
-    file_name_with_brackets <- str_glue("[{file_name}]")
-    
-    cat(file_name_with_brackets, sep = "\n")
-    
-  }
   
 }
