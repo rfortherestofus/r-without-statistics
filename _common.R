e48ac919aefec70ccce54bd2bdca06527571b53a
David Keyes <david@rfortherestofus.com>
2022-08-15 11:35:02 -0700

    Rendering of Rmd and files all created for No Starch

--- _common.R
+++ _common.R
@@ -16,16 +16,12 @@ knitr::opts_chunk$set(
 
 # Functions ---------------------------------------------------------------
 
-i <- 1
-
 create_nostarch_file_name <- function(file_type = "pdf", chap_number = chapter_number, print_figure_number = FALSE) {
   
   chapter_number_two_digits <- stringr::str_pad(chap_number, 2, "left", "0")
   figure_number_three_digits <- stringr::str_pad(i, 3, "left", "0")
   
   file_name <- stringr::str_glue("F{chapter_number_two_digits}{ figure_number_three_digits }.{file_type}")
-  
-  i <<- i + 1
   
   file_name
   
@@ -40,8 +36,6 @@ print_nostarch_file_name <- function(file_type_to_print = "pdf") {
 }
 
 
-create_nostarch_file_name(chap_number = 2)
-
 save_figure_for_nostarch <- function(figure_height = 4) {
   
   save_directory <- here::here("nostarch/figures/")
@@ -53,6 +47,8 @@ save_figure_for_nostarch <- function(figure_height = 4) {
          device = cairo_pdf,
          width = 4.675,
          height = figure_height)
+  
+  i <<- i + 1
 
   
 }
@@ -68,5 +64,7 @@ save_image_for_nostarch <- function(image_file, chap_number = chapter_number) {
   fs::file_copy(path = image_file,
                 new_path = str_glue("{save_directory}/{file_name}"),
                 overwrite = TRUE)
+  
+  i <<- i + 1
   
 }
