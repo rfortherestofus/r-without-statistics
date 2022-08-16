885f78ea84718244359a5674e9c33bd7cbc762a1
David Keyes <david@rfortherestofus.com>
2022-08-16 11:08:58 -0700

    Try to use officer to change figure numbers

--- nostarch/render.R
+++ nostarch/render.R
@@ -6,6 +6,7 @@ library(janitor)
 library(fs)
 library(rmarkdown)
 library(xfun)
+library(officer)
 
 # Copy source files -------------------------------------------------------
 
@@ -25,12 +26,12 @@ gsub_file(file = "nostarch/source/_common.R",
           "print_figure_number = TRUE")
 
 gsub_files(files = dir_ls(path = "nostarch/source", regexp = "Rmd"),
-          "print_nostarch_file_name\\(\\)",
-          "print_nostarch_file_name(actually_print = TRUE)")
+           "print_nostarch_file_name\\(\\)",
+           "print_nostarch_file_name(actually_print = TRUE)")
 
 gsub_files(files = dir_ls(path = "nostarch/source", regexp = "Rmd"),
-          'print_nostarch_file_name\\(file_type_to_print = "png"\\)',
-          'print_nostarch_file_name(file_type_to_print = "png", actually_print = TRUE)')
+           'print_nostarch_file_name\\(file_type_to_print = "png"\\)',
+           'print_nostarch_file_name(file_type_to_print = "png", actually_print = TRUE)')
 
 # Render ------------------------------------------------------------------
 
@@ -46,6 +47,29 @@ file_copy(path = "nostarch/source/_book/r-without-statistics.docx",
           overwrite = TRUE)
 
 file_show("nostarch/word/r-without-statistics.docx")
+
+
+# Replace figure x.x with x-x in Word doc ---------------------------------
+
+# word_doc <- read_docx("nostarch/word/r-without-statistics.docx")
+# 
+# updated_word_doc <- body_replace_all_text(word_doc,
+#                                           "Figure ([0-9]*)\\.([0-9]*)",
+#                                           "Figure \\1-\\2")
+# 
+# str_replace("Figure 2.1",
+#             pattern = regex("[0-9]\\.[0-9]"),
+#             replacement = "test")
+# 
+# fig <- "Figure 127.34"
+# stringr::str_replace(fig, "Figure ([0-9]*)\\.([0-9]*)", "Figure \\1-\\2")
+# 
+# str_replace(fig, "\\[\\.\\]", "test")
+# 
+# styles_info(word_doc)
+# 
+# print(updated_word_doc,
+#       target = "nostarch/word/new.docx")
 
 
 # Manual Rendering --------------------------------------------------------
