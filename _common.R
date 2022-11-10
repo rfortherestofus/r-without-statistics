0f806a689f35549440ddf0363a1933449104a17e
David Keyes <david@rfortherestofus.com>
2022-11-10 07:54:38 -0800

    Trying to figure out table rendering as images with GH actions

--- _common.R
+++ _common.R
@@ -53,8 +53,10 @@ print_nostarch_file_name <- function(file_type_to_print = "pdf", output_format =
 
 # * Tables ----------------------------------------------------------------
 
-save_table_for_nostarch <- function(table_object) {
-
+save_table_for_nostarch <- function(table_object, output_format = "html") {
+  
+  if (output_format == "word") {
+    
     file_name <- create_nostarch_file_name(file_type = "png")
     file_name_with_path <- str_glue(here::here("nostarch/temp/{file_name}"))
     
@@ -64,6 +66,17 @@ save_table_for_nostarch <- function(table_object) {
     save_image_for_nostarch(file_name_with_path) 
     
     knitr::include_graphics(file_name_with_path)
+    
+  }
+  
+  if (output_format == "html") {
+    
+    gtsave(data = table_object,
+           filename = "table.png")
+    
+    knitr::include_graphics("table.png")
+    
+  }
   
 }
 
