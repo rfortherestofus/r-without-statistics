b39e7acdf3f71cf34a4f697ed77b7ac05a2d3cfe
David Keyes <david@rfortherestofus.com>
2023-06-06 09:01:59 -0700

    Add back tidycensus function

--- examples/render-examples.R
+++ examples/render-examples.R
@@ -1,7 +1,8 @@
 library(rmarkdown)
 
 # render(input = "examples/penguins-report.Rmd")
-render(input = "examples/xaringan-example.Rmd")
+# render(input = "examples/xaringan-example.Rmd")
 # render(input = "examples/googlesheets-example.Rmd")
+render(input = "examples/tidycensus-function.Rmd")
 
 beepr::beep()
